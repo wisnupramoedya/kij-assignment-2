c405660d8237e0e36fd2229c57ec3c7eeda3f005
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2022-10-30 11:31:45 +0700

    feat: change arc4 to rsa pcks1_oaep

--- services/encryption.py
+++ services/encryption.py
@@ -1,31 +1,32 @@
-from Crypto.Cipher import ARC4
-from Crypto.Hash import SHA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
 
 nonce = b"\x1aB!}\xc6Z8J\xce\x00\xc7\xdd\x9ag\x1e\xd1"
 
 
 class Encryption:
     @staticmethod
-    def get_cipher(key: bytes, nonce: bytes) -> ARC4:
-        temp_key = SHA.new(key + nonce).digest()
-        return ARC4.new(temp_key)
+    def get_cipher(key_filepath: str) -> PKCS1_OAEP:
+        key = RSA.importKey(open(key_filepath).read())
+        return PKCS1_OAEP.new(key)
 
     @staticmethod
-    def encrypt(data: bytes, key: bytes) -> bytes:
-        cipher = Encryption.get_cipher(key, nonce)
-        return nonce + cipher.encrypt(data)
+    def encrypt(data: bytes, key_filepath: str) -> bytes:
+        cipher = Encryption.get_cipher(key_filepath)
+        return cipher.encrypt(data)
 
     @staticmethod
-    def decrypt(data: bytes, key: bytes) -> bytes:
-        cipher = Encryption.get_cipher(key, data[:16])
-        return cipher.decrypt(data[16:])
+    def decrypt(data: bytes, key_filepath: str) -> bytes:
+        cipher = Encryption.get_cipher(key_filepath)
+        return cipher.decrypt(data)
 
 
-key = "Very long and confidential key".encode()
+public_key_filepath = "E:\Sekolah\Tugas Kuliah\Semester 7\KIJ\Assignment 2\sign-and-verif-app\\testcase\\test-public-key.pem"
+private_key_filepath = "E:\Sekolah\Tugas Kuliah\Semester 7\KIJ\Assignment 2\sign-and-verif-app\\testcase\\test-private-key.pem"
 plain_text = "Hello Signing".encode()
 
 print(plain_text)
-cipher_text: bytes = Encryption.encrypt(plain_text, key)
+cipher_text: bytes = Encryption.encrypt(plain_text, public_key_filepath)
 print(cipher_text)
-decrypted_text = Encryption.decrypt(cipher_text, key)
+decrypted_text = Encryption.decrypt(cipher_text, private_key_filepath)
 print(decrypted_text)
