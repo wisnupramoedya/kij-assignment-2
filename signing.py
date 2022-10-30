5cec8cf757c9c0f6109c8246e10fce7627554d8a
Stefanus Albert Kosim <albertkosim24@gmail.com>
2022-10-30 19:56:22 +0700

    remove comment

--- signing.py
+++ signing.py
@@ -2,8 +2,3 @@ from views.gui_signing import gui_on
 
 
 gui_on()
-
-# data = read pdf
-# hashed_data = hash(data)
-# signature = encrypt(hashed_data)
-# dig_sign_pdf = attach(signature, data)
--- verificating.py
+++ verificating.py
@@ -2,11 +2,3 @@ from views.gui_verificating import gui_on
 
 
 gui_on()
-
-# parsed = parse(digital_signed_pdf)
-# original_pdf = parsed[0]
-# signature = parsed[1]
-# hash = decrypt(signature)
-# original_hash = hash(original_pdf)
-# if hash == original_hash:
-#   print(verified)
